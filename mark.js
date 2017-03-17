9d888a6e0bd469f729664caa44a0c6776999fb67
ife <598978359@qq.com>
2017-03-17 17:16:23 +0800

    2017.3.17 Fourth
    鼠标点击

--- mark.js
+++ mark.js
@@ -14,8 +14,8 @@ window.onload = function() {
 	
 	mark(row);
 	addText(row);
-	//mouseClick(row);
-	showLayout(row);
+	mouseClick(row);
+	showContent();
 }
 
 function mark(row) {
@@ -28,31 +28,48 @@ function mark(row) {
 			e.cancelBubble = true;
 			e.preventDefault();
 			e.stopPropagation();
+			var t = parseInt($('#textarea').css('top'))/15;
+			//文本域下移
+			$('#textarea').css('top', (t + 1) * 15);
+			$('#textarea').val("");
 			// 添加行
 			var div = document.createElement('div');
 			div.setAttribute('class', 'rows');
 			var span = document.createElement('span');
 			div.appendChild(span);
-			$('.row-list')[0].appendChild(div);
+			//$('.row-list')[0].appendChild(div);
+			$('.rows:eq(' + t + ')').after(div);
 			row.push(div);
-			for (var i=0; i<row.length; i++) {
-				row[i].style.top = i*15 + 'px';
-			}
-			$('#textarea').css('top', (row.length-1)*15);
-			$('#textarea').val("");
 			// 添加列表数字
 			$('.row-num').append('<div class="nums">' + row.length + '</div>');
+			//添加右侧div
+			$('.output:eq(0)').append('<div class="outext"></div>');
 		}
 		
 		// backspace
 		if (e.keyCode == 8) {
 			var nowt = parseInt($('#textarea').css('top'));
 			var i = $('span:eq(' + nowt/15 + ')').html();
-			if (i == '') {
+			var val = $('span:eq('+ (nowt/15-1) + ')').html(val);
+			if (i == '' && nowt/15 != 0) {
+				//拦截删除
+				e.cancelBubble = true;
+				e.preventDefault();
+				e.stopPropagation();
+				//删除行
 				$('.rows:eq(' + nowt/15 + ')').remove();
+				//文本域上移
 				$('#textarea').css('top', nowt - 15);
+				$('#textarea').val(val);
+				//删除数组中指定位置
 				row.splice(nowt/15, 1);
-				$('.nums:eq(' + nowt/15 + ')').remove();
+				//刷新数字
+				$('.nums').remove();
+				for (var i=0; i<row.length; i++) {
+					$('.row-num').append('<div class="nums">' + (i+1) + '</div>');
+				}		
+				//删除右侧
+				$('.outext:eq(' + nowt/15 + ')').remove();
 			}
 		}
 		
@@ -86,27 +103,29 @@ function addText(row) {
 	});
 }
 
-//监听up down enter back，刷新布局
-function showLayout(row) {
-	$('#textarea').keyup(function(e) {
-		var e = e || window.event;
-		if (e.keyCode == 13 || e.keyCode == 38 || e.keyCode == 40  || e.keyCode == 8) {
-			row.each(function(index) {
-				$('.output').append('<div class=' + index + '></div>');
-			});
-		}
-	})
-}
-
-
 //监听addtext，刷新内容
 function showContent() {
-	
+	$('#textarea').keyup(function() {
+		var t = parseInt($('#textarea').css('top'))/15;
+		var val = $('span:eq(' + t + ')').html();
+		$('.outext:eq(' + t + ')').html(val);
+	});
 }
 
 //鼠标点击
-//function mouseClick(row) {
-//}
+function mouseClick(row) {
+	addEventListener('click', function(e) {
+		var e = e || window.event;
+		var t = Math.floor(e.clientY / 15);
+		var val = $('span:eq(' + t +')').html();
+		console.log(t);
+		console.log(val);
+		if (t <= row.length) {
+			$('#textarea').css('top', t*15);
+			$('#textarea').val(val);
+		}
+	});
+}
 
 //正则编译
 function regex() {
